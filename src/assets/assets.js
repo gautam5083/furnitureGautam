12d1e313934fd061f4fab79e2fafaa98e910fd7f
gautam solanki <solankigautam2960@gmail.com>
2025-12-02 21:56:57 +0530

    img

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,5 @@
 import bg from "./Bg.png"
 import img1 from "./img1.png"
-import img2 from "./image.png"
 import img3 from "./living.png"
 import img4 from "./living2.png"
 import img5 from "./living3.png"
@@ -12,5 +11,5 @@ import img8 from "./img8.png"
 
 
 export const assets ={
-    img1,bg,img2,img3,img4,img5,img6,img9,img7,img8
+    img1,bg,img3,img4,img5,img6,img9,img7,img8
 }
